479497293dbe776876b0ad61fec5e66155399ab9
rkparashar <parasharragh@gmail.com>
2013-01-27 02:00:00 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobalert::Application.config.secret_token = '7d56d7ce7bdfd2ed77915f71db54a962c77901b54560ebdc5a019021d58e26247ec1820b8f62edd6aea0f1900b99c1ac140a3cbd3bae79d0a95cf65c974a5542'
